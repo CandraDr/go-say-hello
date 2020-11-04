9706b19c783d76bc013b681af0b1e30892f9413f
CandraDr <Darmawancandra@Gmail.com>
2020-11-04 21:34:15 +0700

    update say hello with parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/CandraDr/go-say-hello
+module github.com/CandraDr/go-say-hello/v2
 
 go 1.15
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello candra darmawan"
+func SayHello(name string) string {
+	return "Hello " + name
 }
